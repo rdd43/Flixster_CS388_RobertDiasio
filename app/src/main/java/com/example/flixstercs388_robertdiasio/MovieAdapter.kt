01ed275112d2fce6af80093b324247033e854adf
Robert Diasio <rdd43@njit.edu>
2022-09-28 16:24:52 -0400

    Placeholder

--- app/src/main/java/com/example/flixstercs388_robertdiasio/MovieAdapter.kt
+++ app/src/main/java/com/example/flixstercs388_robertdiasio/MovieAdapter.kt
@@ -1,14 +1,21 @@
 package com.example.flixstercs388_robertdiasio
 
 import android.content.Context
+import android.content.Intent
+import android.transition.Transition
+import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
+private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: MutableList<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
@@ -24,16 +31,35 @@ class MovieAdapter(private val context: Context, private val movies: MutableList
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
 
         private val tvPoster = itemView.findViewById<ImageView>(R.id.tvPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
 
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         fun bind(movie: Movie){
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
-            Glide.with(context).load(movie.posterImageUrl).placeholder(R.drawable.placeholder).into(tvPoster)
+            val radius = 30
+            val margin = 30
+            Glide.with(context)
+                .load(movie.posterImageUrl)
+                .transform(RoundedCorners(radius))
+                .placeholder(R.drawable.placeholder)
+                .into(tvPoster)
+        }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+            //Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 }
